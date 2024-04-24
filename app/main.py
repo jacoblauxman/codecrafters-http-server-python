11710a87b6d5c02740a1d866a490a398ecd847a9
Jacob Lauxman <jlauxman@gmail.com>
2024-04-23 21:43:46 -0700

    stage 2 attempt 2

--- app/main.py
+++ app/main.py
@@ -1,6 +1,5 @@
 import socket
 
-
 def main():
 
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
@@ -10,11 +9,24 @@ def main():
 
 
 def handler(stream):
-    stream.recv(4096)
+    while True:
 
-    http_res = "HTTP/1.1 200 OK\r\n\r\n"
+        req_data = stream.recv(4096).decode()
+        if not req_data:
+            break
 
-    stream.send(http_res.encode())
+        req_lines = req_data.split("\r\n")
+        method, path, version = req_lines[0].split(" ")
+
+        if path == "/":
+            http_res = "HTTP/1.1 200 OK\r\n\r\n"
+
+            stream.send(http_res.encode())
+        else:
+            http_not_found = "HTTP/1.1 404 NOT FOUND\r\n\r\n"
+            stream.send(http_not_found.encode())
+
+        stream.close()
 
 
 if __name__ == "__main__":
