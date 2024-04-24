c8631e31ac368ec9ba027fcbe39c59ce3dba541a
Jacob Lauxman <jlauxman@gmail.com>
2024-04-23 23:58:02 -0700

    stage 8 attempt

--- app/main.py
+++ app/main.py
@@ -25,8 +25,10 @@ def route_handler(stream, _addr, directory):
             return
 
         req_lines = req_data.split("\r\n")
+        print(f"REQ LINES:\n{req_lines}")
         method, path, version = req_lines[0].split(" ")
         user_agent = req_lines[2]
+        body_content = req_lines[-1]
 
         if path == "/":
             http_res = "HTTP/1.1 200 OK\r\n\r\n"
@@ -41,6 +43,10 @@ def route_handler(stream, _addr, directory):
             if method == "GET":
                 get_file_res = get_file_handler(path, directory)
                 stream.send(get_file_res.encode())
+            elif method == "POST":
+                pass
+                post_file_res = post_file_handler(path, directory, body_content)
+                stream.send(post_file_res.encode())
         else:
             http_not_found = "HTTP/1.1 404 NOT FOUND\r\n\r\n"
             stream.send(http_not_found.encode())
@@ -54,7 +60,7 @@ def echo_handler(path):
     content_type = "Content-Type: text/plain"
     content_len = f"Content-Length: {len(echo_str)}"
     status_line = "HTTP/1.1 200 OK"
-    http_res = f"{status_line}\r\n{content_type}\r\n{content_len}\r\n\r\n{echo_str}\r\n\r\n"
+    http_res = f"{status_line}\r\n{content_type}\r\n{content_len}\r\n\r\n{echo_str}"
 
     return http_res
 
@@ -64,7 +70,7 @@ def user_agent_handler(user_agent):
     content_type = "Content-Type: text/plain"
     content_len = f"Content-Length: {len(ua)}"
     status_line = "HTTP/1.1 200 OK"
-    http_res = f"{status_line}\r\n{content_type}\r\n{content_len}\r\n\r\n{ua}\r\n\r\n"
+    http_res = f"{status_line}\r\n{content_type}\r\n{content_len}\r\n\r\n{ua}"
 
     return http_res
 
@@ -79,13 +85,22 @@ def get_file_handler(path, directory):
             content_type = "Content-Type: application/octet-stream"
             content_len = f"Content-Length: {len(file_content)}"
             status_line = "HTTP/1.1 200 OK"
-            http_res = f"{status_line}\r\n{content_type}\r\n{content_len}\r\n\r\n{file_content}\r\n\r\n"
+            http_res = f"{status_line}\r\n{content_type}\r\n{content_len}\r\n\r\n{file_content}"
 
             return http_res
     else:
         http_not_found = "HTTP/1.1 404 NOT FOUND\r\n\r\n"
 
         return http_not_found
+
+def post_file_handler(path, directory, content):
+    filename = path.split("/files/")[-1]
+    file_path = os.path.join(directory, filename)
+
+    with open(file_path, "w") as file:
+        file.write(content)
+
+    return "HTTP/1.1 201 CREATED\r\n\r\n"
 
 
 if __name__ == "__main__":
