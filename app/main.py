bfdc2639fad60dcab137a9521db08e1823178bb2
Jacob Lauxman <jlauxman@gmail.com>
2024-04-23 21:28:20 -0700

    stage 2 attempt

--- app/main.py
+++ app/main.py
@@ -4,7 +4,17 @@ import socket
 def main():
 
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept() # wait for client
+    stream, _addr = server_socket.accept() # wait for client
+
+    handler(stream)
+
+
+def handler(stream):
+    stream.recv(4096)
+
+    http_res = "HTTP/1.1 200 OK\r\n\r\n"
+
+    stream.send(http_res.encode())
 
 
 if __name__ == "__main__":
