42aa533a7acea3041fee42d42fc45a5f4e816891
Jacob Lauxman <jlauxman@gmail.com>
2024-04-23 23:41:49 -0700

    stage 7 attempt

--- app/main.py
+++ app/main.py
@@ -1,17 +1,23 @@
+import argparse
+import os
 import socket
 from threading import Thread
 
 def main():
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--directory", help="File serving directory")
+    args = parser.parse_args()
+
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
 
     while True:
         stream, _addr = server_socket.accept() # wait for client
-        # route_handler(stream)
-        thread = Thread(target=route_handler, args=(stream,))
+        thread = Thread(target=route_handler, args=(stream, _addr, args.directory))
         thread.start()
 
 
-def route_handler(stream):
+def route_handler(stream, _addr, directory):
 
     while True:
         req_data = stream.recv(4096).decode()
@@ -29,9 +35,12 @@ def route_handler(stream):
             echo_res = echo_handler(path)
             stream.send(echo_res.encode())
         elif path.startswith("/user-agent"):
-            # ua_res = user_agent_handler(req_lines)
             ua_res = user_agent_handler(user_agent)
             stream.send(ua_res.encode())
+        elif path.startswith("/files"):
+            if method == "GET":
+                get_file_res = get_file_handler(path, directory)
+                stream.send(get_file_res.encode())
         else:
             http_not_found = "HTTP/1.1 404 NOT FOUND\r\n\r\n"
             stream.send(http_not_found.encode())
@@ -50,7 +59,6 @@ def echo_handler(path):
     return http_res
 
 def user_agent_handler(user_agent):
-    print(f"HERE: {user_agent}")
     _ua_header, ua = user_agent.split(": ")
 
     content_type = "Content-Type: text/plain"
@@ -60,6 +68,24 @@ def user_agent_handler(user_agent):
 
     return http_res
 
+def get_file_handler(path, directory):
+    filename = path.split("/files/")[-1]
+
+    file_path = os.path.join(directory, filename)
+    if os.path.isfile(file_path):
+        with open(file_path, "r") as file:
+            file_content = file.read()
+
+            content_type = "Content-Type: application/octet-stream"
+            content_len = f"Content-Length: {len(file_content)}"
+            status_line = "HTTP/1.1 200 OK"
+            http_res = f"{status_line}\r\n{content_type}\r\n{content_len}\r\n\r\n{file_content}\r\n\r\n"
+
+            return http_res
+    else:
+        http_not_found = "HTTP/1.1 404 NOT FOUND\r\n\r\n"
+
+        return http_not_found
 
 
 if __name__ == "__main__":
